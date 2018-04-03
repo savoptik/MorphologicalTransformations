a804d635cf5327386f710b461ed0ae88cb06d7d4
savoptik <sav70792@gmail.com>
2018-04-04 00:47:40 +0300

    Добавленно изображение. Добавлены файлы под базовый класс.

---
+++ Morphological transformations/morphologicalTransformations/morphologicalTransformations.cpp
@@ -0,0 +1,9 @@
+//
+//  morphologicalTransformations.cpp
+//  Morphological transformations
+//
+//  Created by Артём Семёнов on 04.04.2018.
+//  Copyright © 2018 Артём Семёнов. All rights reserved.
+//
+
+#include "morphologicalTransformations.hpp"
---
+++ Morphological transformations/morphologicalTransformations/morphologicalTransformations.hpp
@@ -0,0 +1,15 @@
+//
+//  morphologicalTransformations.hpp
+//  Morphological transformations
+//
+//  Created by Артём Семёнов on 04.04.2018.
+//  Copyright © 2018 Артём Семёнов. All rights reserved.
+//
+
+#ifndef morphologicalTransformations_hpp
+#define morphologicalTransformations_hpp
+
+#include <stdio.h>
+
+
+#endif /* morphologicalTransformations_hpp */
