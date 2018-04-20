c972803b90a18d3c2ab6f0ea5c1fc923fa92dbdb
savoptik <sav70792@gmail.com>
2018-04-20 18:24:50 +0300

    описана структура класса.

---
+++ Morphological transformations/CreatorКing/CreatorКing.cpp
@@ -0,0 +1,9 @@
+//
+//  CreatorКing.cpp
+//  Morphological transformations
+//
+//  Created by Артём Семёнов on 20.04.2018.
+//  Copyright © 2018 Артём Семёнов. All rights reserved.
+//
+
+#include "CreatorКing.hpp"
