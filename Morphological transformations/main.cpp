f7a65545533972ef8bb6df20f426b6d84d34ea01
savoptik <sav70792@gmail.com>
2018-05-04 19:29:15 +0300

    Выполнено.

--- Morphological transformations/main.cpp
+++ Morphological transformations/main.cpp
@@ -19,22 +19,19 @@ const string filePath = "./media/";
 
 int main(int argc, const char * argv[]) {
     morphologicalTransformationsBase image("./media/Шестеренки.png"); // загрузка исходного сообщения.
-    /*    // umage.showCurrentVersion(); // вывод исходного изображения для контроля.
+    image.showCurrentVersion(); // вывод исходного изображения для контроля.
     holeRing closeHoloring(image.extractImage()); // создание объекта, который будет закрывать дырки.
     closeHoloring.closingHolis(97); // закрытие дыок.
-    closeHoloring.showCurrantImage(); // вывод изображения. */
-    morphologicalTransformationsBase st2(filePath + "result1.jpg"); // загрузка шестерёнки с закрытыми внутренними отверстиями.
-    st2.showCurrentVersion(); // вывод изображения для контроля.
-    CreatorRing cr(st2.extractImage()); // загрузка матрицы в объект, который вырежет внутренний диск.
+    CreatorRing cr(closeHoloring.extracktIMG()); // загрузка матрицы в объект, который вырежет внутренний диск.
     cr.disconnection(280); // вырезание внутреннего диска.
     cr.showResult(); // вывод результата.
     allocationDefects ad(cr.exportResult()); // загрузка матрицы в объект, который маркирует здоровые зубцы.
     ad.labelWholeTeeth(10); // маркирование здоровых зубцов.
     ad.showResult(); // вывод результата.
-    morphologicalTransformationsBase st3(filePath + "Шестеренки.png");
-    markingDefects md(st3.extractImage());
+    markingDefects md(image.extractImage());
     md.markingDifectTeeth(cr.extractInternalCircul(), ad.exportResultMatrix());
     md.showResultImage();
+    image.exportToDisk(filePath, md.exportResult());
     return 0; // выход.
 }
 
